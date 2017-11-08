86034f950a794889bbdbaf297d4b2aa4d9b10f49
Levi Roth <levimroth@gmail.com>
2017-11-07 21:00:18 -0500

    Successfully extracting a (string * string) list

--- main.ml
+++ main.ml
@@ -9,6 +9,25 @@ open Longident
    Then we can use that list as the basis for constructing the rest of the code.
 *)
 
+(* These functions should preserve loc *)
+let extract_tuple : Parsetree.expression -> string * string =
+  function
+  | {pexp_desc = Pexp_tuple [{pexp_desc = Pexp_ident ({txt = Lident field_name})};
+                             {pexp_desc = Pexp_ident ({txt = Lident type_name})}];} ->
+    field_name, type_name
+  | {pexp_loc} -> raise @@ Location.Error (Location.error ~loc:pexp_loc "Expected two identifiers")
+
+let rec extract_list : Parsetree.expression -> (string * string) list =
+  function
+  | {pexp_desc = Pexp_construct ({txt = Lident "::"; loc}, Some ({pexp_desc = Pexp_tuple [hd; tl]})) } -> extract_tuple hd :: extract_list tl
+  | {pexp_desc = Pexp_construct ({txt = Lident "::"; loc}, None) } -> []
+  | _ -> raise @@ invalid_arg "foo"
+
+let extract_payload : Parsetree.payload -> (string * string) list =
+  function
+  | PStr [{pstr_desc = Pstr_eval (expr, _)}] -> extract_list expr
+  | _ -> raise @@ invalid_arg "foo"
+
 let build_field (name, t_name) ~loc =
   let t = Typ.constr {txt = (Lident t_name); loc} [] in
   Type.field { txt = name ; loc} @@ t
