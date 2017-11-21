3a18b408db54d92d48812d5791f2ca5234b025c0
Levi Roth <levimroth@gmail.com>
2017-11-20 23:55:12 -0500

    Simplify example

--- example/bmp_header.ml
+++ example/bmp_header.ml
@@ -11,7 +11,6 @@ type t = {
 let () =
   let f = open_in_bin "smiley.bmp" in
   let buf = Bytes.create 14 in
-  match input f buf 0 14 with
-  | 14 -> let my_t = of_bytes buf 0 in
-    print_endline @@ Printf.sprintf "bfType: %s" @@ Uint16.to_string_hex my_t.bfType
-  | _ -> assert false
+  let _ = input f buf 0 14 in
+  let my_t = of_bytes buf 0 in
+  print_endline @@ Printf.sprintf "bfType: %s" @@ Uint16.to_string_hex my_t.bfType
